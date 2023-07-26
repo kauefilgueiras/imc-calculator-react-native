14ae53542f21a00662322c22e4f45528f036920e
kauefilgueiras <kaue.filgueiras@contele.com.br>
2023-07-26 16:38:31 -0300

    first style

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import Title from './src/components/Title';
-import Main from './src/components/Main';
+import Form from './src/components/Form';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Title/>
-      <Main/>
+      <Form/>
     </View>
   );
 }
@@ -14,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#E5E5E5',
+    paddingTop: 80,
   },
 });
---
+++ src/components/Form/ResultImc/style.js
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  ResultImc: {
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    alignItems: "center",
+    width: "100%",
+    fontSize: 48,
+    color: "#FF0043",
+    fontWeight: "bold",
+  },
+  numberImc: {
+    fontSize: 48,
+    color:"#FF0043",
+    fontWeight: "bold"
+  },
+  information: {
+    fontSize: 18,
+    color: "#FF0043",
+    fontWeight: "bold"
+  },
+  contextImc: {
+    flex: 0,
+    marginTop: 15,
+    paddingTop: 60,
+    borderRadius: 50,
+    alignItems: "center",
+    width: "100%",
+  }
+})
+
+export default styles
